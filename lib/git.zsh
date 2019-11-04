bbbefb43ec37e661ba1bbbf3e7d093c5b67b1ea9
Ben <github@benvan.co.uk>
2019-11-04 14:57:39 +0000

    git prompt has tag in it

--- lib/git.zsh
+++ lib/git.zsh
@@ -4,7 +4,15 @@ function git_prompt_info() {
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+		echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_tag)$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  fi
+}
+
+function git_prompt_tag(){
+  local tag=$(git rev-list --tags --max-count=1)
+  if [[ "${tag}" ]]; then
+    tag=$(git describe --tags ${tag})
+    echo "%{$fg_no_bold[white]%}|%{$fg_no_bold[green]%}${tag}"
   fi
 }
 

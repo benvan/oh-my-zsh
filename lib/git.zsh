05dbbc579a89b01b924c22e13b01da359600712b
Ben <github@benvan.co.uk>
2020-02-07 11:57:59 +0000

    feat: git prompt shows warning for posterity commits

--- lib/git.zsh
+++ lib/git.zsh
@@ -4,7 +4,7 @@ function git_prompt_info() {
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-		echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_tag)$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+		echo "$(git_prompt_posterity)$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_tag)$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
 
@@ -12,8 +12,14 @@ function git_prompt_tag(){
   local tag=$(git rev-list --tags --max-count=1)
   if [[ "${tag}" ]]; then
     tag=$(git describe --tags ${tag})
-    echo "%{$fg_no_bold[white]%}|%{$fg_no_bold[green]%}${tag}"
+		echo "%{$fg_no_bold[white]%}|%{$fg_no_bold[green]%}${tag}"
   fi
+}
+
+function git_prompt_posterity(){
+	if git log --pretty=%B -1 | grep -o posterity > /dev/null; then
+    echo "%{$fg_no_bold[red]%}[[--POSTERITY--]]%{$reset_color%}"
+	fi
 }
 
 # Checks if working tree is dirty
